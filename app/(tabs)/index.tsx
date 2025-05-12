1bf3ab5ea2827397db0b129af20400758e7fc62f
VIVEK KUMAR <vivkumar4798@gmail.com>
2025-05-13 00:28:34 +0530

    logo updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,27 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { ScrollView, Text, View } from "react-native";
 import { Image } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router =  useRouter();
+
   return (
     <View
        className="flex-1 bg-primary">
         <Image source={images.bg}
           className="absolute w-full z-0"/>
 
-        <ScrollView className="flex-1 px-5">
+        <ScrollView className="flex-1 px-5"
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
           <Image source={icons.logo} 
                   className="w-24 h-16 mt-20 mx-auto"/>
+          <View className="flex-1 mt-5">
+            <SearchBar/>
+          </View>
         </ScrollView>
 
     </View>
