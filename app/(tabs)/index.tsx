d20358bee23604438bd80ef3e3dc316c65e5c0a7
VIVEK KUMAR <vivkumar4798@gmail.com>
2025-05-12 10:29:36 +0530

    routing done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { ScrollView, Text, View } from "react-native";
+import { Image } from "react-native";
 
 export default function Index() {
   return (
     <View
-       className="flex-1 justify-center items-center"
-    >
-      <Text 
-        className="text-5xl text-primary font-bold"
-        >Welcome!
-        </Text>
+       className="flex-1 bg-primary">
+        <Image source={images.bg}
+          className="absolute w-full z-0"/>
+
+        <ScrollView className="flex-1 px-5">
+          <Image source={icons.logo} 
+                  className="w-24 h-16 mt-20 mx-auto"/>
+        </ScrollView>
+
     </View>
   );
 }
