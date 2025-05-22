668a31f38ecd04af01fbca023470e29fa8aa94f2
VIVEK KUMAR <vivkumar4798@gmail.com>
2025-05-22 17:38:02 +0530

    tasks yet to do added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,15 @@ import { Stack } from "expo-router";
 import './globals.css';
 import { StatusBar } from "react-native";
 
+// sthings yet to do
+// 1. add a  login screen 
+// 2. add a logout screen
+// 3. add a register screen
+// 4. add a profile screen
+// 5. add a settings screen
+// 6. add a saved movies screen
+
+
 export default function RootLayout() {
   return <>
         <StatusBar hidden={true}/>
@@ -11,7 +20,7 @@ export default function RootLayout() {
         options={{headerShown: false}}  
       />
       <Stack.Screen
-        name="movie/[id]"
+        name="movies/[id]"
         options={{headerShown: false}}  
       />
     </Stack>
